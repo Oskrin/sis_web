3c5bfe2df2c742e3f83a551d30e2016d13b8d09f
Oskrin <oskrs11@hotmail.com>
2015-05-29 12:04:51 -0500

    cambios

--- data/index.php
+++ data/index.php
@@ -5,7 +5,8 @@
     <title>LOGIN</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../../font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />        
+    <link href="../../plugins/icon/ionicons.min.css" rel="stylesheet" type="text/css" />    
     <link href="../dist/css/AdminLTE.min.css" rel="stylesheet" type="text/css" />
     <link href="../dist/css/alertify.core.css" rel="stylesheet" />
     <link href="../dist/css/alertify.default.css" id="toggleCSS" rel="stylesheet" />
@@ -35,20 +36,20 @@
                 </label>
               </div>                        
             </div><!-- /.col -->
-            <div class="col-xs-4">
+            <div class="col-xs-8">
               <button type="submit" class="btn btn-primary btn-block btn-flat" id="btnIngreso" >INGRESAR</button>
             </div><!-- /.col -->
-            <div class="col-xs-4">
+            <!-- <div class="col-xs-4">
               <button type="submit" class="btn btn-primary btn-block btn-flat" id="btnRetornar">RETORNAR</button>
-            </div>
+            </div> -->
           </div>
         </form>
 
-        <div class="social-auth-links text-center">
+       <!--  <div class="social-auth-links text-center">
           <p>- OR -</p>
           <a href="" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>Facebook</a>
           <a href="" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i>Google+</a>
-        </div>
+        </div> -->
       </div><!-- /.login-box-body -->
     </div><!-- /.login-box -->
 
